20e9c11845a13d32b010e873646c2890f9b822ad
LElTEDEV <zinenizi@gmail.com>
2026-02-24 17:59:25 -0300

    refactor: adicionado font bold no text

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,11 @@
+import { fontFamily } from "@/theme/font-family";
 import { router } from "expo-router";
 import { Button, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View>
-      <Text>Olá, expo router.</Text>
+      <Text style={{ fontFamily: fontFamily.bold }}>Olá, expo router.</Text>
       <Button title="Meta" onPress={() => router.navigate("/target")} />
     </View>
   );
