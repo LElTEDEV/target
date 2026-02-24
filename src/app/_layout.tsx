129433198fb53c3d4bcd2f1c86db0c784c4d19f9
LElTEDEV <zinenizi@gmail.com>
2026-02-24 17:53:37 -0300

    feat: adicionado o carregamento das fontes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,22 @@
 import { colors } from "@/theme/colors";
 import { Stack } from "expo-router";
 
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_700Bold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+
 export default function Layout() {
+  const [fontLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold,
+  });
+
+  if (!fontLoaded) return;
+
   return (
     <Stack
       screenOptions={{
