1f0f74b10d5939dd5d88621e87740f37b99d4078
LElTEDEV <zinenizi@gmail.com>
2026-02-25 15:52:10 -0300

    feat: adicionado page-header e input component

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,15 +1,23 @@
-import { router } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { Button } from "@/components/button";
+import { Input } from "@/components/input";
+import { PageHeader } from "@/components/page-header";
+import { Text, View } from "react-native";
 
 export default function Target() {
   return (
-    <View>
-      <Text>Target</Text>
-      <Button
+    <View style={{ flex: 1, padding: 24 }}>
+      <PageHeader
         title="Meta"
-        onPress={() => router.navigate("/transactions/2502")}
+        subtitle="Economize para alcançar sua meta financeira."
       />
-      <Button title="Voltar" onPress={() => router.back()} />
+
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <Input
+          label="Nome da meta"
+          placeholder="Ex: Viagem para praia, Apple Watch"
+        />
+        <Button title="Salvar" />
+      </View>
     </View>
   );
 }
