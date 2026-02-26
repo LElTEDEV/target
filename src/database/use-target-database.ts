e57525552fd0b776206d7083639af439a646f0d4
LElTEDEV <zinenizi@gmail.com>
2026-02-26 14:43:28 -0300

    feat: criado funcao show

--- src/database/use-target-database.ts
+++ src/database/use-target-database.ts
@@ -52,5 +52,24 @@ export function useTargetDatabase() {
     `);
   }
 
-  return { create, listBySavedValue };
+  async function show(id: number) {
+    return await database.getFirstAsync<TargetResponse>(`
+        SELECT
+          targets.id,
+          targets.name,
+          targets.amount,
+          COALESCE(SUM(transactions.amount), 0) AS current,
+          COALESCE((SUM(transactions.amount) / targets.amount) * 100, 0) AS percentage,
+          targets.created_at,
+          targets.updated_at
+        FROM 
+          targets
+        LEFT JOIN
+          transactions ON transactions.target_id = targets.id
+        WHERE
+          targets.id = ${id}
+    `);
+  }
+
+  return { show, create, listBySavedValue };
 }
